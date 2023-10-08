f03a8bde4d9b437e7decaa9ccd7a62311378a7b1
Priyanshu Thapliyal <priyanshuthapliyal2005@gmail.com>
2023-10-08 14:53:49 +0530

    .

--- assignment/a1/q2.cpp
+++ assignment/a1/q2.cpp
@@ -39,7 +39,8 @@ public:
 
         if (totalBill > 300) {
             return static_cast<int>(totalBill + 0.15 * totalBill);
-        } else {
+        } 
+        else {
             return static_cast<int>(totalBill);
         }
     }
