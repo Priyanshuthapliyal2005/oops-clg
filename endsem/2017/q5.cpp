fc1eae9d3735d75abd43379acd19ae1daf4f5380
Priyanshu Thapliyal <priyanshuthapliyal2005@gmail.com>
2024-01-03 23:31:09 +0530

    diff

--- endsem/2017/q5.cpp
+++ endsem/2017/q5.cpp
@@ -2,20 +2,25 @@
 #include<fstream>
 
 using namespace std;
+
 int main()
 {
     ifstream in1("a.txt");
     ifstream in2("b.txt");
     ofstream out("c.txt");
+
     string content1,content2;
     int cnt=0,cnt1=0;
+
     while(!in1.eof() && !in2.eof())
     {
 
         getline(in1,content1);
         getline(in2,content2);
+        
         out<<content1<<endl;
         out<<content2<<endl;
     }
+
     return 0;
 }
